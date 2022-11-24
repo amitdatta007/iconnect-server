1641c35c2e7377614b2dec2a926cc17fe6320005
Amit Datta <ak.datta2648@gamil.com>
2022-11-24 22:52:48 +0600

    jwt token send

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -23,8 +24,31 @@ const run = async() => {
     try{
         const usersCollection = client.db('iconnect').collection('users');
 
+        app.get('/jwt', async(req, res) => {
+            const email = req.query.email;
+            const query = {
+                email: email,
+            };
+            const user = await usersCollection.findOne(query);
+
+            if(user){
+                const token = jwt.sign({email}, process.env.ACCESS_TOKEN);
+                return res.send({accessToken: token});
+            };
+            res.send({accessToken: ''});
+        });
+
         app.post('/users', async(req, res) => {
             const user = req.body;
+
+            const query = {email: user.email};
+
+            const alreadyHave = await usersCollection.findOne(query);
+
+            if(alreadyHave){
+                return res.send(alreadyHave);
+            };
+
             const result = await usersCollection.insertOne(user);
             res.send(result);
         });
